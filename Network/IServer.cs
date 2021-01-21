0c0c451e5a87de3f13a66f20f31faff35a71cd7e
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2021-01-21 17:34:28 +0200

    -

--- Network/IServer.cs
+++ Network/IServer.cs
@@ -4,14 +4,14 @@ namespace Network
 {
 	public interface IServer
 	{
-		public static int MaxPlayers { get; private set; }
-		public static int Port { get; private set; }
+		public static int MaxPlayers { get; protected set; }
+		public static int Port { get; protected set; }
 		public static Dictionary<int, IClientBase> Clients = new Dictionary<int, IClientBase>();
 		public delegate void PacketHandler(int _fromClient, Packet _packet);
 		public static Dictionary<int, PacketHandler> packetHandlers;
 
-		public abstract void Start(int _maxPlayers, int _port);
+		public void Start(int _maxPlayers, int _port);
 
-		protected abstract void InitializeServerData();
+		public void InitializeServerData();
 	}
 }
