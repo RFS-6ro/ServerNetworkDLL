40cf9b639fa6a69921479788eb1c76602974dca0
RFS-6ro <56915388+RFS-6ro@users.noreply.github.com>
2021-02-19 11:37:59 +0200

    lobby port was added

--- Network/NetworkSettings.cs
+++ Network/NetworkSettings.cs
@@ -6,6 +6,7 @@ namespace Network
 		public const int DATA_BUFFER_SIZE = 4096;
 		public const int PORT = 26950;
 		public const int TEST_SERVERPORT = 26951;
+		public const int LOBBY_CONNECTION_PORT = 26952;
 
 		public const string LOCAL_HOST_IP = "192.168.0.131";
 	}
